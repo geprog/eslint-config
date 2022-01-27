4bf2fbb818c836f38b9577125f15d482f3b5d09a
Lukas <lukas@slucky.de>
2022-01-27 23:33:26 +0100

    test: add test package and workflow (#41)
    * test: add test package and workflow
    
    * typecheck ignore test package
    
    * test eslint output with diff
    
    * test vue rules

---
+++ test/Test.vue
@@ -0,0 +1,17 @@
+<template>
+  <span>{{ foo }}</span>
+  <span>test</span>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  setup() {
+    const foo = 'bar';
+    return { foo };
+  },
+
+  name: 'Test',
+});
+</script>
